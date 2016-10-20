0a46ef7e5b8f2ce2213cc949c8f62d8dc13f8869
jatinrev <jatin@revinfotech.com>
2016-10-20 15:57:13 +0530

    guru-manage-class skeleton ready.

---
+++ src/app/guru-manage-class/guru-manage-class.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GuruManageClassComponent } from './guru-manage-class.component';
+
+describe('Component: GuruManageClass', () => {
+  it('should create an instance', () => {
+    let component = new GuruManageClassComponent();
+    expect(component).toBeTruthy();
+  });
+});
